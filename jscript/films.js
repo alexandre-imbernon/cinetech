964d6d59b4f1c314b16fb46b36e33f3c99e659dd
Adel Grim <adel.grim@laplateforme.iogit config --global adel.grim@laplateforme.io>
2024-04-22 16:13:55 +0200

    ajoute de pagination sur 3 pages

--- jscript/films.js
+++ jscript/films.js
@@ -1,16 +1,18 @@
-function getFilms() {
+function getFilms(page = 1) {
     const apiKey = '8c4b867188ee47a1d4e40854b27391ec';
-    const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=fr&page=1`;
+    const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=fr&page=${page}`;
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             const filmsContainer = document.getElementById('moviesContainer');
+            filmsContainer.innerHTML = ''; // Efface le contenu précédent
+            
             data.results.forEach(film => {
                 const card = document.createElement('div');
-                card.classList.add('col-md-4', 'mb-4');
+                card.classList.add('col-md-3', 'mb-4');
                 card.innerHTML = `
-                    <div class="card">
+                    <div class="card h-100">
                         <img src="https://image.tmdb.org/t/p/w500${film.poster_path}" class="card-img-top" alt="${film.title}">
                         <div class="card-body">
                             <h5 class="card-title">${film.title}</h5>
@@ -21,6 +23,24 @@ function getFilms() {
                 `;
                 filmsContainer.appendChild(card);
             });
+
+            // Pagination
+            const pagination = document.getElementById('pagination');
+            pagination.innerHTML = '';
+            const totalPages = 3; // Nombre total de pages de pagination
+            for (let i = 1; i <= totalPages; i++) {
+                const li = document.createElement('li');
+                li.classList.add('page-item');
+                const link = document.createElement('a');
+                link.classList.add('page-link');
+                link.href = '#';
+                link.textContent = i;
+                link.addEventListener('click', () => {
+                    getFilms(i); // Appel de la fonction avec le numéro de page correspondant
+                });
+                li.appendChild(link);
+                pagination.appendChild(li);
+            }
         })
         .catch(error => console.error('Erreur lors de la récupération des films :', error));
 }
