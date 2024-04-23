34df0d32be70be0ec953da20786cdc0f20193319
alexandre-imbernon <alexandre.imbernon@laplateforme.io>
2024-04-23 11:09:44 +0200

    adding of director fetch through api

--- jscript/details.js
+++ jscript/details.js
@@ -6,32 +6,39 @@ if (!serieId) {
 
 const apiKey = '8c4b867188ee47a1d4e40854b27391ec';
 const apiUrl = `https://api.themoviedb.org/3/tv/${serieId}?api_key=${apiKey}&language=fr`;
+const creditsUrl = `https://api.themoviedb.org/3/tv/${serieId}/credits?api_key=${apiKey}&language=fr`;
 
 // Appel à l'API TMDB pour récupérer les détails de la série
 fetch(apiUrl)
-    .then(response => {
-        if (!response.ok) {  // Vérifiez si la réponse est OK
-            throw new Error('Erreur lors de la récupération des détails de la série');
-        }
-        return response.json();  // Assurez-vous de retourner le JSON pour définir data
-    })
-    .then(data => {  // Maintenant vous pouvez utiliser data
+    .then(response => response.json())
+    .then(data => {
         const serieTitle = data.name;  
         const genres = data.genres.map(genre => genre.name).join(', ');
 
-        document.getElementById("details").innerHTML = `
-            <h2>${serieTitle}</h2>
-            <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="${serieTitle}">
-            <p>${data.overview}</p>
-            <p>Nombre de saisons : ${data.number_of_seasons}</p>
-            <p>Nombre d'épisodes : ${data.number_of_episodes}</p>
-            <p>Genre : ${genres}</p>
-            <p>Réalisateur : </p>
-            <p>Acteurs :</p>
-            <p>Nationalité : </p>
-        `;
+        // Appel à l'API TMDB pour récupérer les crédits de la série
+        fetch(creditsUrl)
+            .then(response => response.json())
+            .then(creditsData => {
+                const director = creditsData.crew.find(member => member.job === 'Director');
+                const directorName = director ? director.name : 'Inconnu';
 
-        document.getElementById("serieliked").innerText = serieTitle;
+                document.getElementById("details").innerHTML = `
+                    <h2>${serieTitle}</h2>
+                    <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" alt="${serieTitle}">
+                    <p>${data.overview}</p>
+                    <p>Nombre de saisons : ${data.number_of_seasons}</p>
+                    <p>Nombre d'épisodes : ${data.number_of_episodes}</p>
+                    <p>Genre : ${genres}</p>
+                    <p>Réalisateur : ${directorName}</p>
+                    <p>Acteurs :</p>
+                    <p>Nationalité : </p>
+                `;
+
+                document.getElementById("serieliked").innerText = serieTitle;
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des crédits de la série :', error);
+            });
     })
     .catch(error => {
         console.error('Erreur lors de la récupération des détails de la série :', error);
